6e3c0fcd87f2856a312d6724b1593193cd3ce174
isur3n <surendra.raut@gmail.com>
2019-10-31 23:00:37 -0400

    Initial check-in.

---
+++ src/main/java/com/isur3n/api/confluence/ConfluenceApplication.java
@@ -0,0 +1,13 @@
+package com.isur3n.api.confluence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfluenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfluenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isur3n/api/confluence/ConfluenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isur3n.api.confluence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfluenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
